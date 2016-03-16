0434c7245b453926e8fa90d7de7e9a6c482eb420
liaozd <liao_zd@hotmail.com>
2016-03-16 22:33:18 +0800

    re schema database design

--- src/scanner.py
+++ src/scanner.py
@@ -21,23 +21,23 @@ ffmpeg -i [SourcePath] -vcodec copy -acodec copy -timecode 1:23:45:01 [DestPath]
 
 
 def read_clip_meta(clips):
-    try:
-        """
-        Use command get clip meta:
-        ffprobe -print_format json -show_format -show_streams [SourcePath]
-        """
-        cmd = ['ffprobe', '-v', 'quiet', '-print_format', 'json',
-               '-show_format', '-show_streams', clips]
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        data = json.loads(proc.communicate()[0])
-        data_needed = {'timecode': data['streams'][-1]['tags']['timecode'],
-                       'duration': data['streams'][-1]['duration'],
-                       'all_meta': data,
-                       }
-        return data_needed
-    except Exception:
-        return None
+    # try:
+    """
+    Use command get clip meta:
+    ffprobe -print_format json -show_format -show_streams [SourcePath]
+    """
+    cmd = ['ffprobe', '-v', 'quiet', '-print_format', 'json',
+           '-show_format', '-show_streams', clips]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    data = json.loads(proc.communicate()[0])
+    data_needed = {'tc_in': data['streams'][-1]['tags']['timecode'],
+                   'duration': data['streams'][-1]['duration'],
+                   'meta': data,
+                   }
+    return data_needed
+    # except Exception:
+    #     return None
 
 
 class Scanner(object):
@@ -53,35 +53,39 @@ class Scanner(object):
         self.c.execute('DROP TABLE IF EXISTS TRACKS;')
         """
         CAM_ID:
-        TC: timecode in the metadata
+        TC_IN: in point, get from the metadata
+        TC_OUT: out point, calculated
         DURATION: duration in the metadata
         FIR_F: first frame calculated by timecode, map to fcp xml <start>
         LAST_F: last frame calculated by `first frame` + `duration`, map to
                 fcp xml <end>
-        ALLMETA: all metadata
+        META: all metadata
         FULLPATH: file full path on disk
         """
         self.c.execute('CREATE   TABLE    TRACKS'
                        '(ID      INTEGER  PRIMARY KEY AUTOINCREMENT,'
                        'CAM_ID   CHAR(100)            NOT NULL,'
-                       'TC       CHAR(11)             NOT NULL,'
-                       'DURATION INT                  NOT NULL,'
+                       'TC_IN    CHAR(11)             NOT NULL,'
+                       'TC_OUT   CHAR(11)             NOT NULL,'
                        'FIR_F    INT                  NOT NULL,'
                        'LAST_F   INT                  NOT NULL,'
-                       'ALLMETA  TEXT                 NOT NULL,'
+                       'DURATION INT                  NOT NULL,'
+                       'META     TEXT                 NOT NULL,'
                        'FULLPATH CHAR(300)            NOT NULL);')
         self.conn.commit()
 
     # TODO use dict pass parameters
-    def insert_record(self, cam_id, tc, duration, allmeta, fullpath):
-        fir_f = Timecode(FRAMERATE, tc).frames
+    def insert_record(self, cam_id, tc_in, duration, meta, fullpath):
+        fir_f = Timecode(FRAMERATE, tc_in).frames
         last_f = fir_f + duration
+        tc_out = '0'
         sql = ('INSERT INTO TRACKS '
-               '(CAM_ID,TC,DURATION,FIR_F,LAST_F,ALLMETA,FULLPATH) VALUES '
-               '("{CAM_ID}","{TC}","{DURATION}","{FIR_F}","{LAST_F}",'
-               '"{ALLMETA}","{FULLPATH}");'.
-               format(CAM_ID=cam_id, TC=tc, DURATION=duration, FIR_F=fir_f,
-                      LAST_F=last_f, ALLMETA=allmeta, FULLPATH=fullpath))
+               '(CAM_ID,TC_IN, TC_OUT,DURATION,FIR_F,LAST_F,META,FULLPATH) '
+               'VALUES ("{CAM_ID}","{TC_IN}","{TC_OUT}","{FIR_F}","{LAST_F}",'
+               '"{DURATION}","{META}","{FULLPATH}");'.
+               format(CAM_ID=cam_id, TC_IN=tc_in, TC_OUT=tc_out, FIR_F=fir_f,
+                      LAST_F=last_f, DURATION=duration, META=meta,
+                      FULLPATH=fullpath))
         self.c.execute(sql)
 
     def scan(self, path):
@@ -101,9 +105,9 @@ class Scanner(object):
                         if data:
                             frames = int(float(data['duration']) * FRAMERATE)
                             self.insert_record(cam_id=camera_id,
-                                               tc=data['timecode'],
+                                               tc_in=data['tc_in'],
                                                duration=frames,
-                                               allmeta=data,
+                                               meta=data,
                                                fullpath=fullpath)
                             print 'Insert clip: {0}'.format(fullpath)
                         else:
