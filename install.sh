6233ef52a162383aa94fad2980e085b2da649159
liaozd <liao_zd@hotmail.com>
2016-09-05 22:11:36 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install ffmpeg
+brew install -y git ffmpeg
+sudo easy_install pip
+
+cd $HOME
+git clone https://github.com/liaozd/melissa
+cd melissa/
+pip install -r requirements.txt -i https://pypi.douban.com/simple/
+
+
