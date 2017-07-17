717cf0490963b533500c84788ef3fcd638ed6440
Liao Zhuodi <liao_zd@hotmail.com>
2017-07-17 12:41:22 +0800

    fix hardlink recursive make full path of source

--- src/tools/hardlink.sh
+++ src/tools/hardlink.sh
@@ -2,12 +2,22 @@
 
 me=`basename "$0"`
 
-echo $? $1 $2
 if [ "$#" -lt 2 ]; then
   echo "Usage: $me Source_DIR Link_DIR"
   exit 1
 fi
 
-echo "Create hardlink for diretroy $1"
-mkdir $2
-pax -rwlpe $1 $2
+# Prepare
+src=$1
+src_path_name=`basename $src`
+src_parent=$(dirname $src)
+dest=$2
+mkdir -p $dest
+current=${PWD}
+
+# Hardlink recursively
+echo "Create hardlink for diretroy $1 TO: $2"
+relative_to_src=`realpath --relative-to=src_parent $2`
+cd $src_parent
+pax -rwlpe $src_path_name $dest
+cd $current_dir
