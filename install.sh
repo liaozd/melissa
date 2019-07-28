0efe04d14c1c2b80d34767525b8b2022673088f2
leo <liao_zd@hotmail.com>
2019-07-28 19:10:32 +0800

    Update install.sh
    update for macOS 10.14.4

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install -y git ffmpeg
+brew install git ffmpeg
 sudo easy_install pip
 
 cd $HOME
